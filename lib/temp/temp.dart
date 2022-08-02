cadc1756eeb3663e2f9d30a004354d318c6edaa8
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-08-02 14:16:03 +0545

    temp page created

---
+++ lib/temp/temp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TempPage extends StatelessWidget {
+  const TempPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('title'),
+      ),
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
